7c9e4a6ebdb8935e132832138250e7b2d68a6d83
ramaadi <17980909+rama-adi@users.noreply.github.com>
2024-06-13 07:26:53 +0700

    done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
                 <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Dokumentasi</a>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-                <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
+                <a href="{{route('login')}}" class="text-sm font-semibold leading-6 text-gray-900">Log in <span
                         aria-hidden="true">&rarr;</span></a>
             </div>
         </nav>
